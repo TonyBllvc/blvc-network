9bc9f7be1281369aa71b1e29ef8e2b052e7be488
TonyBllvc <george.ekwemadu@gmail.com>
2025-12-19 12:06:00 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Blvc Network </title>
+</head>
+<body>
+    
+    <h1>
+        Welcome to the Black Network
+    </h1>
+    <p> Click the button below to view the list of Back </p>
+
+    <a href="" class="btn">
+        Find Blacks
+    </a>
+
+</body>
+</html>
